b776589bbc7c29d3ef4bbb84534fd69a89e5d7c8
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2026-03-22 15:22:56 -0300

    modifico service de idecor

--- app/services/idecor_service.py
+++ app/services/idecor_service.py
@@ -1,4 +1,5 @@
 import requests
+from shapely.geometry import shape
 
 WFS_URL = "https://idecor-ws.mapascordoba.gob.ar/geoserver/idecor/parcelas/wfs"
 
@@ -10,7 +11,7 @@ def buscar_parcela_por_cuenta(numero: str):
         "request": "GetFeature",
         "typeName": "idecor:parcelas",
         "outputFormat": "application/json",
-        "CQL_FILTER": f"Nro_Cuenta='{numero}'"
+        "CQL_FILTER": f"NRO_CUENTA='{numero}'"
     }
 
     response = requests.get(WFS_URL, params=params)
@@ -20,7 +21,7 @@ def buscar_parcela_por_cuenta(numero: str):
 
     data = response.json()
 
-    if not data["features"]:
+    if not data.get("features"):
         return None
 
     feature = data["features"][0]
@@ -28,15 +29,61 @@ def buscar_parcela_por_cuenta(numero: str):
     geometry = feature["geometry"]
     props = feature["properties"]
 
-    # calcular centroide (simple)
-    coords = geometry["coordinates"][0][0]
-    lon = sum(p[0] for p in coords) / len(coords)
-    lat = sum(p[1] for p in coords) / len(coords)
+    # centroid real
+    geom = shape(geometry)
+    centroid = geom.centroid
 
     return {
         "geometry": geometry,
-        "latitude": lat,
-        "longitude": lon,
-        "area": props.get("superficie"),
+        "latitude": centroid.y,
+        "longitude": centroid.x,
+        "area": props.get("SUP_TERR") or props.get("superficie"),
+        "nomenclatura": props.get("NOMENCLATURA"),
+        "designacion": props.get("DESIGNACION"),
         "properties": props
     }
+
+
+
+# import requests
+
+# WFS_URL = "https://idecor-ws.mapascordoba.gob.ar/geoserver/idecor/parcelas/wfs"
+
+# def buscar_parcela_por_cuenta(numero: str):
+
+#     params = {
+#         "service": "WFS",
+#         "version": "1.0.0",
+#         "request": "GetFeature",
+#         "typeName": "idecor:parcelas",
+#         "outputFormat": "application/json",
+#         "CQL_FILTER": f"Nro_Cuenta='{numero}'"
+#     }
+
+#     response = requests.get(WFS_URL, params=params)
+
+#     if response.status_code != 200:
+#         return None
+
+#     data = response.json()
+
+#     if not data["features"]:
+#         return None
+
+#     feature = data["features"][0]
+
+#     geometry = feature["geometry"]
+#     props = feature["properties"]
+
+#     # calcular centroide (simple)
+#     coords = geometry["coordinates"][0][0]
+#     lon = sum(p[0] for p in coords) / len(coords)
+#     lat = sum(p[1] for p in coords) / len(coords)
+
+#     return {
+#         "geometry": geometry,
+#         "latitude": lat,
+#         "longitude": lon,
+#         "area": props.get("superficie"),
+#         "properties": props
+#     }
